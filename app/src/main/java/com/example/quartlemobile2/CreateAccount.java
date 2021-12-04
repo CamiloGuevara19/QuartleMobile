720c40db5c44eb75407306b454fe0ae0d620d92e
CamiloGuevara19 <camilogr0419@gmail.com>
2021-12-03 23:48:38 -0500

    Firebase implemented

--- app/src/main/java/com/example/quartlemobile2/CreateAccount.java
+++ app/src/main/java/com/example/quartlemobile2/CreateAccount.java
@@ -3,18 +3,25 @@ package com.example.quartlemobile2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class CreateAccount extends AppCompatActivity {
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CreateAccount extends AppCompatActivity implements View.OnClickListener {
 
     private EditText registerInput1, registerInput2, registerInput3, registerInput4;
     private Button registerBtn;
+
+    private FirebaseDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        db = FirebaseDatabase.getInstance();
 
         //References
         registerInput1 = findViewById(R.id.registerInput1);
@@ -22,5 +29,15 @@ public class CreateAccount extends AppCompatActivity {
         registerInput3 = findViewById(R.id.registerInput3);
         registerInput4 = findViewById(R.id.registerInput4);
         registerBtn = findViewById(R.id.registerBtn);
+
+        registerBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.registerBtn:
+                    break;
+        }
     }
 }
--- app/src/main/java/com/example/quartlemobile2/MainActivity.java
+++ app/src/main/java/com/example/quartlemobile2/MainActivity.java
@@ -2,25 +2,49 @@ package com.example.quartlemobile2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText loginInput1, loginInput2;
     private Button logInBtn1, loginBtn2;
 
+    private FirebaseDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //References
+        db = FirebaseDatabase.getInstance();
 
+        //References
         loginInput1 = findViewById(R.id.loginInput1);
         loginInput2 = findViewById(R.id.loginInput2);
         logInBtn1 = findViewById(R.id.logInBtn1);
         loginBtn2 = findViewById(R.id.loginBtn2);
+
+        logInBtn1.setOnClickListener(
+                (view)-> {
+
+                    Intent dashboardIntent = new Intent(this, Dashboard.class);
+                    startActivity(dashboardIntent);
+
+                }
+        );
+
+        loginBtn2.setOnClickListener(
+                (view)-> {
+
+                    Intent registerIntent = new Intent(this, CreateAccount.class);
+                    startActivity(registerIntent);
+
+                }
+        );
     }
 }
