53ef14064f4c5dee4fa40ccf06e4dc53ce1bbb39
CamiloGuevara19 <camilogr0419@gmail.com>
2021-12-04 20:52:12 -0500

    scanner init

--- app/src/main/java/com/example/quartlemobile2/Scanner.java
+++ app/src/main/java/com/example/quartlemobile2/Scanner.java
@@ -3,12 +3,34 @@ package com.example.quartlemobile2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Scanner extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+
+public class Scanner extends AppCompatActivity implements View.OnClickListener {
+
+    private Button scannerBtn;
+    private EditText scannerResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        scannerBtn = findViewById(R.id.scannerBtn);
+        scannerResult = findViewById(R.id.scannerResult);
+
+        scannerBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.scannerBtn:
+                IntentIntegrator inte
+                break;
+        }
     }
 }
