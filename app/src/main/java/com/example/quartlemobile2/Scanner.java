71ca36bfd7dbb4cd3847d2b49c88a6990ce7b724
CamiloGuevara19 <camilogr0419@gmail.com>
2021-12-04 21:33:59 -0500

    QR code scanner implemented

--- app/src/main/java/com/example/quartlemobile2/Scanner.java
+++ app/src/main/java/com/example/quartlemobile2/Scanner.java
@@ -2,12 +2,15 @@ package com.example.quartlemobile2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class Scanner extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,8 +32,30 @@ public class Scanner extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.scannerBtn:
-                IntentIntegrator inte
+                IntentIntegrator integrador = new IntentIntegrator(Scanner.this);
+                integrador.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrador.setPrompt("Lector - CDP");
+                integrador.setCameraId(0);
+                integrador.setBeepEnabled(true);
+                integrador.setBarcodeImageEnabled(true);
+                integrador.initiateScan();
                 break;
+        }
+    }
+
+    protected void onActivityResult(int requestCode,int resultCode, Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result != null){
+            if(result.getContents() == null){
+                Toast.makeText(this, "Lectura cancelada", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+                scannerResult.setText(result.getContents());
+            }
+        } else{
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 }
